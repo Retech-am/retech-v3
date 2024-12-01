eb5e8cf0b1fb7fd46fb11011d2bfe20c2ef819e2
Retech <retech.am@gmail.com>
2024-12-01 18:44:29 -0400

    Update pedidos.js

--- backend/routes/pedidos.js
+++ backend/routes/pedidos.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db');
 const jwt = require('jsonwebtoken');
-const secretKey = 'seu-segredo-seguro'; // Substitua pela sua chave secreta
+const secretKey = 'seu-segredo-seguro';
 
 // Middleware para verificar token JWT e adicionar cargo do usuário
 function authenticateToken(req, res, next) {
